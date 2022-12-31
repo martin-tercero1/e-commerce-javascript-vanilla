4e1aa274f92750d884ec8cc6ace43aae4dd8203c
martin-tercero1 <martintercero20@hotmail.com>
2022-12-31 14:51:19 -0800

    Combine the mobile menu with the desktop menu

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const navbarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const hamburguerMenu = document.querySelector('.menu-icon');
 
 navbarEmail.addEventListener('click', toggleDesktopMenu);
+hamburguerMenu.addEventListener("click", toggleMobileMenu);
 
 function toggleDesktopMenu() {
-    console.log('click');
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    console.log('click');
+    mobileMenu.classList.toggle('inactive');
 }
