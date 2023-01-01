5dc22162d6d07b012bd4c3ee3f592015ffd6ed7c
martin-tercero1 <martintercero20@hotmail.com>
2022-12-31 20:56:41 -0800

    Add shopping cart. Control behavior of other menus

--- main.js
+++ main.js
@@ -2,15 +2,32 @@ const navbarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const hamburguerMenu = document.querySelector('.menu-icon');
+const cartIcon = document.querySelector('.navbar-shopping-cart');
+const shoppingCart = document.querySelector('.my-order');
 
 navbarEmail.addEventListener('click', toggleDesktopMenu);
-hamburguerMenu.addEventListener("click", toggleMobileMenu);
+hamburguerMenu.addEventListener('click', toggleMobileMenu);
+cartIcon.addEventListener('click', toggleShoppingCart);
 
 function toggleDesktopMenu() {
+    if (!shoppingCart.classList.contains('inactive')) {
+        shoppingCart.classList.add("inactive");
+    }
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
-    console.log('click');
-    mobileMenu.classList.toggle('inactive');
+    if (!shoppingCart.classList.contains("inactive")) {
+      shoppingCart.classList.toggle("inactive");
+    }
+    mobileMenu.classList.toggle("inactive");
+}
+
+function toggleShoppingCart() {
+    if (!desktopMenu.classList.contains('inactive')) {
+        desktopMenu.classList.add("inactive");
+    } else if (!mobileMenu.classList.contains('inactive')) {
+        mobileMenu.classList.add("inactive");
+    }
+    shoppingCart.classList.toggle('inactive');
 }
